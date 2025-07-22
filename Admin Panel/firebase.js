1c5ea32845ca75f5cd45ba6f9f0f2ce346a3c3cf
agasthya1990 <agasthyarocker@gmail.com>
2025-07-23 04:17:59 +0530

    Update firebase.js

--- Admin Panel/firebase.js
+++ Admin Panel/firebase.js
@@ -4,14 +4,15 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
-const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_STORAGE_BUCKET",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
-};
+ // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyD4Pob4ftpSkA0Tn22KShkinIniWiOv5IQ",
+    authDomain: "gufa-restaurant.firebaseapp.com",
+    projectId: "gufa-restaurant",
+    storageBucket: "gufa-restaurant.firebasestorage.app",
+    messagingSenderId: "105496307977",
+    appId: "1:105496307977:web:f9e61bf7ccc09ac5c7cbd8"
+  };
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
