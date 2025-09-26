438a15e6fad41c02586824033f2731ccc560a260
agasthya1990 <agasthyarocker@gmail.com>
2025-09-27 01:33:22 +0530

    Update firebase.js

--- admin/firebase.js
+++ admin/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 import { getStorage } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-storage.js";
 
- // Your web app's Firebase configuration
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyD4Pob4ftpSkA0Tn22KShkinIniWiOv5IQ",
   authDomain: "gufa-restaurant.firebaseapp.com",
   projectId: "gufa-restaurant",
-  storageBucket: "gufa-restaurant.firebasestorage.app",, // ✅ Add this line
+  storageBucket: "gufa-restaurant.firebasestorage.app",
   messagingSenderId: "105496307977",
   appId: "1:105496307977:web:f9e61bf7ccc09ac5c7cbd8"
 };
